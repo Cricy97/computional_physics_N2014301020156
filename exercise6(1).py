078c16f9fa94f0470f24b8563f486d1adf9b0d3a
Cricy97 <908517294@qq.com>
2016-10-27 18:37:43 +0800

    Create exercise6(1).py

---
+++ exercise6(1).py
@@ -0,0 +1,40 @@
+import math
+import matplotlib.pyplot as plt  
+from matplotlib import animation      
+g=9.8 
+#calculate the trajectory
+def Damped(omega0,theta0,q,l,T):#q is related to damping force, while FD and omegaD is related to the driving force. the length of the rod is l.T is the interest time.
+    dt=0.001
+    t=0
+    omega,theta = omega0,theta0
+    motion=[[]for i in range(3)]
+    motion[0].append(omega)
+    motion[1].append(theta)
+    motion[2].append(t)
+    while t<= T:
+        omega = omega+(-g*theta/l-q*omega)*dt
+        theta = theta+omega*dt
+        t = t+dt
+        motion[0].append(omega)
+        motion[1].append(theta)
+        motion[2].append(t)
+    return motion
+
+#Fig.1.damped pendulum without driving force
+d=DampedDriven(0,0.5,1,1,10)
+plt.plot(d[2],d[1],linestyle='-',linewidth=1.0,label='q=1')
+d=DampedDriven(0,0.5,3,1,10)
+plt.plot(d[2],d[1],linestyle='-',linewidth=1.0,label='q=3')
+d=DampedDriven(0,0.5,9.8**0.5*2,1,10)
+plt.plot(d[2],d[1],linestyle='-',linewidth=1.0,label='q='+r'$2\sqrt{g/l}$')
+d=DampedDriven(0,0.5,10,1,10)
+plt.plot(d[2],d[1],linestyle='-',linewidth=1.0,label='q=10')
+d=DampedDriven(0,0.5,30,1,10)
+plt.plot(d[2],d[1],linestyle='-',linewidth=1.0,label='q=30')
+plt.xlim(0,10)
+plt.grid(True,color='k')
+plt.title('Fig.1 Damped Pendulum')
+plt.xlabel('Time(s)')
+plt.ylabel(r'$\theta$(radius)')
+plt.legend()
+plt.show()
